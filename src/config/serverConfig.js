0f1c93cec93ca3e25270a456beea418ea62f0640
siddhi Algude <algudesiddhi04@gmail.com>
2024-03-15 19:00:49 +0530

    setup of SMTP using nodemailer

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -1,7 +1,10 @@
-const dotenv= require('dotenv');
+const dotenv = require('dotenv');
 
 dotenv.config();
 
 module.exports = {
-   PORT:process.env.PORT
-}
+    PORT: process.env.PORT,
+    EMAIL_ID: process.env.EMAIL_ID,
+    EMAIL_PASS: process.env.EMAIL_PASS,
+    
+};
--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
-const express=require('express');
+const express = require('express');
 const bodyParser = require('body-parser');
-const {PORT }= require('./config/serverConfig');
+
+const { PORT } = require('./config/serverConfig');
+const {sendBasicEmail} = require('./services/email-service');
 
 
-const setUpAndStartServer = ( ) => {
+const setUpAndStartServer = ( ) => {  
   const app = express();
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended:true}));
 
   app.listen(PORT,()=>{
     console.log(`server started at ${PORT}`);
-  });
+    sendBasicEmail(
+      'airlinebooking004@gmail.com',
+      'algudesiddhi@gmail.com',
+      'this is a testing email ',
+      'Hey  how are you ? be happy you are going to be rich soon there are lot of opportunities for youu! take care in timing bbyee'
+    );
+ });
 }
-
+  
 setUpAndStartServer();
