91798b668f045fa5509402c1041caef97f673385
siddhi Algude <algudesiddhi04@gmail.com>
2024-03-16 10:28:08 +0530

    created a email remainder service

--- src/index.js
+++ src/index.js
@@ -2,22 +2,27 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
-const {sendBasicEmail} = require('./services/email-service');
-
+//const {sendBasicEmail} = require('./services/email-service');
+ 
+const TicketController =  require('./controllers/ticket-controller')
+const jobs =  require('./utils/job');
 
 const setUpAndStartServer = ( ) => {  
-  const app = express();
+  const app = express(); 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended:true}));
-
-  app.listen(PORT,()=>{
+  app.post('/api/v1/tickets',TicketController.create);
+  
+  app.listen(PORT,()=>{ 
     console.log(`server started at ${PORT}`);
-    sendBasicEmail(
-      'airlinebooking004@gmail.com',
-      'algudesiddhi@gmail.com',
-      'this is a testing email ',
-      'Hey  how are you ? be happy you are going to be rich soon there are lot of opportunities for youu! take care in timing bbyee'
-    );
+    jobs();
+    // sendBasicEmail(
+    //   'airlinebooking004@gmail.com',
+    //   'algudesiddhi@gmail.com',
+    //   'this is a testing email ',
+    //   'Hey  how are you ? be happy you are going to be rich soon there are lot of opportunities for youu! take care in timing bbyee'
+    // );
+   
  });
 }
   
